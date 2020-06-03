cce8df7ecd48a7d159f215950e1d6d341c8bbfa0
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2020-06-03 19:58:32 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -65,11 +65,11 @@ async def analyze(request):
     cats = ['Abyssinian', 'Bengal', 'Birman', 'Bombay', 'British_Shorthair', 'Egyptian_Mau', 'Maine_Coon', 'Persian', 'Ragdoll', 'Russian_Blue', 'Siamese', 'Sphynx']
     dogs = ['american_bulldog', 'american_pit_bull_terrier', 'basset_hound', 'beagle', 'boxer', 'chihuahua', 'english_cocker_spaniel', 'english_setter', 'german_shorthaired', 'great_pyrenees', 'havanese', 'japanese_chin', 'keeshond', 'leonberger', 'miniature_pinscher', 'newfoundland', 'pomeranian', 'pug', 'saint_bernard', 'samoyed', 'scottish_terrier', 'shiba_inu', 'staffordshire_bull_terrier', 'wheaten_terrier', 'yorkshire_terrier']
     animal = None
-    if(str(prediction) in cats):
-        animal = "cat"
-    elif(str(prediction) in dogs):
-        animal = "dog"
-    print(animal) 
+    #if(str(prediction) in cats):
+     #   animal = "cat"
+    #elif(str(prediction) in dogs):
+     #   animal = "dog"
+    #print(animal) 
     return JSONResponse({'The above image is of a {}'.format(str(prediction))})
 
 

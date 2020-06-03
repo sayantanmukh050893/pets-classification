2d1639fdc5cce5d89cb7b395c7710993b9a421d5
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2020-06-03 20:12:07 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -13,7 +13,6 @@ export_file_url = 'https://drive.google.com/u/0/uc?id=1-Nc2rdnc4nQQhfm9-e1c5xqbo
 export_file_name = 'export.pkl'
 
 classes = ['Abyssinian', 'Bengal', 'Birman', 'Bombay', 'British_Shorthair', 'Egyptian_Mau', 'Maine_Coon', 'Persian', 'Ragdoll', 'Russian_Blue', 'Siamese', 'Sphynx', 'american_bulldog', 'american_pit_bull_terrier', 'basset_hound', 'beagle', 'boxer', 'chihuahua', 'english_cocker_spaniel', 'english_setter', 'german_shorthaired', 'great_pyrenees', 'havanese', 'japanese_chin', 'keeshond', 'leonberger', 'miniature_pinscher', 'newfoundland', 'pomeranian', 'pug', 'saint_bernard', 'samoyed', 'scottish_terrier', 'shiba_inu', 'staffordshire_bull_terrier', 'wheaten_terrier', 'yorkshire_terrier']
-#classes = ['Animal : Cat, Breed : Abyssinian', 'Animal : Cat, Breed : Bengal', 'Animal : Cat, Breed: Birman', 'Animal : Cat, Breed : Bombay', 'Animal : Cat, Breed : British Shorthair', 'Animal : Cat, Breed : Egyptian Mau', 'Animal : Cat, Breed : Maine Coon', 'Animal : Cat, Breed : Persian', 'Animal : Cat, Breed : Ragdoll', 'Animal : Cat, Breed : Russian Blue', 'Animal : Cat, Breed : Siamese', 'Animal : Cat, Breed : Sphynx', 'Animal : Dog, Breed : American Bulldog', 'Animal : Dog, Breed : American Pit Bull Terrier', 'Animal : Dog, Breed : Basset Hound', 'Animal : Dog, Breed : Beagle', 'Animal : Dog, Breed : Boxer', 'Animal : Dog, Breed : Chihuahua', 'Animal : Dog, Breed : English Cocker Spaniel', 'Animal : Dog, Breed : English Setter', 'Animal : Dog, Breed : German Shorthaired', 'Animal : Dog, Breed : Great Pyrenees', 'Animal : Dog, Breed : Havanese', 'Animal : Dog, Breed : Japanese Chin', 'Animal : Dog, Breed : Keeshond', 'Animal : Dog, Breed : Leonberger', 'Animal : Dog, Breed : Miniature Pinscher', 'Animal : Dog, Breed : Newfoundland', 'Animal : Dog, Breed : Pomeranian', 'Animal : Dog, Breed : Pug', 'Animal : Dog, Breed : Saint Bernard', 'Animal : Dog, Breed : Samoyed', 'Animal : Dog, Breed : Scottish Terrier', 'Animal : Dog, Breed : Shiba Inu', 'Animal : Dog, Breed : Staffordshire Bull Terrier', 'Animal : Dog, Breed : Wheaten Terrier', 'Animal : Dog, Breed : Yorkshire Terrier']
 path = Path(__file__).parent
 
 app = Starlette()
@@ -61,16 +60,8 @@ async def analyze(request):
     img_data = await request.form()
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
-    prediction = learn.predict(img)[0]
-    cats = ['Abyssinian', 'Bengal', 'Birman', 'Bombay', 'British_Shorthair', 'Egyptian_Mau', 'Maine_Coon', 'Persian', 'Ragdoll', 'Russian_Blue', 'Siamese', 'Sphynx']
-    dogs = ['american_bulldog', 'american_pit_bull_terrier', 'basset_hound', 'beagle', 'boxer', 'chihuahua', 'english_cocker_spaniel', 'english_setter', 'german_shorthaired', 'great_pyrenees', 'havanese', 'japanese_chin', 'keeshond', 'leonberger', 'miniature_pinscher', 'newfoundland', 'pomeranian', 'pug', 'saint_bernard', 'samoyed', 'scottish_terrier', 'shiba_inu', 'staffordshire_bull_terrier', 'wheaten_terrier', 'yorkshire_terrier']
-    animal = None
-    #if(str(prediction) in cats):
-     #   animal = "cat"
-    #elif(str(prediction) in dogs):
-     #   animal = "dog"
-    #print(animal) 
-    return JSONResponse({'The above image is of a ',str(prediction)})
+    prediction = learn.predict(img)[0] 
+    return JSONResponse({'Predicted Breed ': str(prediction)})
 
 
 if __name__ == '__main__':

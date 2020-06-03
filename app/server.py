b4943630d3d2a580f550ef85f13683765482802a
Sayantan Mukhopadhyay <59133918+sayantanmukh050893@users.noreply.github.com>
2020-06-03 20:17:54 +0530

    Update server.py

--- app/server.py
+++ app/server.py
@@ -61,7 +61,7 @@ async def analyze(request):
     img_bytes = await (img_data['file'].read())
     img = open_image(BytesIO(img_bytes))
     prediction = learn.predict(img)[0] 
-    return JSONResponse({'Predicted Breed ': str(prediction)})
+    return JSONResponse({'result': str(prediction)})
 
 
 if __name__ == '__main__':
